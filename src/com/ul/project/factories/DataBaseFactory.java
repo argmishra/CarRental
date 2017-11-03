e564794ee69af9c37b2bb50854d727199cd8c53e
Prerus667 <31545049+Prerus667@users.noreply.github.com>
2017-11-03 02:17:19 +0000

    Update DataBaseFactory.java
    Commit made by Prerna  3 /11/2017

--- src/com/ul/project/factories/DataBaseFactory.java
+++ src/com/ul/project/factories/DataBaseFactory.java
@@ -3,13 +3,29 @@ package com.ul.project.factories;
 import java.sql.Connection;
 
 import com.ul.project.utilities.DatabaseUtilities;
+import com.ul.project.utilities.OracleDatabaseUtilities;
 
+// TODO: Auto-generated Javadoc
+/**
+ * A factory for creating DataBase objects.
+ */
 public class DataBaseFactory {
 
+	/**
+	 * Gets the connection.
+	 *
+	 * @param dbType the db type
+	 * @return the connection
+	 * @throws Exception the exception
+	 */
 	public static Connection getConnection(String dbType) throws Exception{
 		Connection connection = null;
 		if(dbType.equalsIgnoreCase("mysql")){
 			connection = DatabaseUtilities.gettingMysqlConnection();
+		}
+		else if(dbType.equalsIgnoreCase("oracle"))
+		{
+			connection=OracleDatabaseUtilities.gettingOracleConnection();
 		}
 		return connection;
 	}
