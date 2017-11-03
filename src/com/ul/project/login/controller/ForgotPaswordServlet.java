5a935dee1269343c9c40e3f45b148fee1cca5367
argmishra <32418194+argmishra@users.noreply.github.com>
2017-11-03 00:30:22 +0000

    Update ForgotPaswordServlet.java

--- src/com/ul/project/login/controller/ForgotPaswordServlet.java
+++ src/com/ul/project/login/controller/ForgotPaswordServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.ul.project.login.LoginAndRegistrationService;
-
+//Reseting Password
 
 public class ForgotPaswordServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
